2a94e158b17f5b06f2e4f500bd1705734b88775c
Alexander Kahl <ak@sodosopa.io>
2016-11-29 20:56:56 +0100

    Added hydrogen

--- audio.nix
+++ audio.nix
@@ -13,6 +13,7 @@ in
       
   environment.systemPackages = with pkgs; [
     jack2Full qjackctl swh_lv2 rkrlv2 metersLv2 mda_lv2 lv2 ams-lv2 ardour pavucontrol
+    hydrogen
   ];
 
   security.pam.loginLimits = [
