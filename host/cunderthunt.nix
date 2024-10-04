7ead983973301e0d8c47eb2def5a230f8290328a
Alexander Dorn <ad@not.one>
2024-10-04 22:19:08 +0200

    add blender for AMD

--- host/cunderthunt.nix
+++ host/cunderthunt.nix
@@ -47,4 +47,10 @@
   swapDevices = [ ];
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  environment = {
+    systemPackages = with pkgs; [
+      blender-hip
+    ];
+  };
 }
